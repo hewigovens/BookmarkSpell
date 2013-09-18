ad9ccdf30c8b861016e79af2882ace2308412bb5
hewigovens <hewigovens@gmail.com>
2013-09-18 09:20:44 +0800

    exclude some files when creating zip

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
+
+ZIP_EXCLUDE="*.DS_Store *.swo *.swp src/*~ src/js/*~ src/css/*~"
+
 tool/crxtool.sh src tool/BookmarkSpell.pem build/BookmarkSpell.crx
-zip -r src.zip src && mv src.zip build/forChromeStore.zip
+zip -r src.zip src -x $ZIP_EXCLUDE && mv src.zip build/forChromeStore.zip
